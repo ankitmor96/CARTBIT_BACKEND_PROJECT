38d659c346b067e605e046f08d6b0b88fec7bd98
ankit9600 <ankitmor1811@gmail.com>
2026-09-09 08:53:45 +0530

    add changes

--- config/redis.js
+++ config/redis.js
@@ -3,6 +3,9 @@ import { createClient } from "redis";
 
 dotenv.config({path:"./.env"});
 
+console.log("REDIS URL EXISTS:", !!process.env.REDIS_URL);
+console.log("REDIS HOST:", process.env.REDIS_URL?.split("@")[1]);
+
 const redisClient = createClient({
     url: process.env.REDIS_URL
 });
